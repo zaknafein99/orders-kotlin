2cd2c98e42eae156b5fa63ab20dd7017180a2ef5
Ismael Marquez <670167+zaknafein99@users.noreply.github.com>
2023-11-02 20:02:06 -0300

    Change method to Add pagination to GET customers by phone number

--- orders/src/main/kotlin/com/kotlin/orders/controller/CustomerController.kt
+++ orders/src/main/kotlin/com/kotlin/orders/controller/CustomerController.kt
@@ -48,7 +48,8 @@ class CustomerController(val customerService : CustomerService, val customerRepo
 
     @GetMapping("/search_phone/{phone_number}")
     @ResponseStatus(HttpStatus.OK)
-    fun getCustomerByPhoneNumber(@PathVariable("phone_number") phoneNumber: String): List<CustomerDTO> {
-        return customerService.getCustomerByPhoneNumber(phoneNumber)
+    fun getCustomerByPhoneNumber(@PathVariable("phone_number") phoneNumber: String, pageable: Pageable): Page<CustomerDTO> {
+        val customers: Page<CustomerDTO> = customerService.getCustomerByPhoneNumber(phoneNumber, pageable)
+        return customers
     }
 }
--- orders/src/main/kotlin/com/kotlin/orders/service/CustomerService.kt
+++ orders/src/main/kotlin/com/kotlin/orders/service/CustomerService.kt
@@ -5,6 +5,9 @@ import com.kotlin.orders.entity.Customer
 import com.kotlin.orders.exceptionhandler.CustomerNotFoundException
 import com.kotlin.orders.repository.CustomerRepository
 import mu.KLogging
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -75,16 +78,16 @@ class CustomerService(val customerRepository: CustomerRepository) {
         }
     }
 
-    fun getCustomerByPhoneNumber(phoneNumber: String): List<CustomerDTO> {
-
+    fun getCustomerByPhoneNumber(phoneNumber: String, pageable: Pageable): Page<CustomerDTO> {
         val existingCustomer = customerRepository.findByPhoneNumber(phoneNumber)
-        return if (existingCustomer.isNotEmpty()) {
+        val customerList = if (existingCustomer.isNotEmpty()) {
             existingCustomer.map {
                 CustomerDTO(it!!.uuid, it.name, it.address, it.phoneNumber, it.type, it.state)
             }
         } else {
             throw CustomerNotFoundException("Customer not found with phone number: $phoneNumber")
         }
+        return PageImpl(customerList, pageable, customerList.size.toLong())
     }
 
     fun getCustomerById(customerId: Int): CustomerDTO {

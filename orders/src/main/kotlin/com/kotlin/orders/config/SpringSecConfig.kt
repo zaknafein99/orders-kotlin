51b99becb43e27198d63eb13f76dd2907ead0265
ismael Marquez <imarquez99@gmail.com>
2023-12-23 22:28:12 -0300

    Move user login data to properties file

--- orders/src/main/kotlin/com/kotlin/orders/config/SpringSecConfig.kt
+++ orders/src/main/kotlin/com/kotlin/orders/config/SpringSecConfig.kt
@@ -1,7 +1,10 @@
 package com.kotlin.orders.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -13,7 +16,11 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:users.properties")
 class SpringSecConfig {
+
+    @Autowired
+    private lateinit var environment: Environment
 
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
@@ -30,17 +37,19 @@ class SpringSecConfig {
     @Bean
     fun userDetailsService(): UserDetailsService {
         val passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
         val userDetails = User.builder()
-            .username("user")
-            .password(passwordEncoder.encode("password"))
-            .roles("USER")
+            .username(environment.getProperty("user.username"))
+            .password(passwordEncoder.encode(environment.getProperty("user.password")))
+            .roles(environment.getProperty("user.roles"))
             .build()
 
         val adminDetails = User.builder()
-            .username("admin")
-            .password(passwordEncoder.encode("adminpassword"))
-            .roles("ADMIN")
+            .username(environment.getProperty("admin.username"))
+            .password(passwordEncoder.encode(environment.getProperty("admin.password")))
+            .roles(environment.getProperty("admin.roles"))
             .build()
+
         return InMemoryUserDetailsManager(userDetails, adminDetails)
     }
 

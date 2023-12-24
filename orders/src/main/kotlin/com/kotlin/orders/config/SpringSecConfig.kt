83cef39787b7bd3a8d6b17dfa5fa0d84525f7c83
ismael Marquez <imarquez99@gmail.com>
2023-12-23 22:03:59 -0300

    Basic authentication, added user and admin

--- orders/src/main/kotlin/com/kotlin/orders/config/SpringSecConfig.kt
+++ orders/src/main/kotlin/com/kotlin/orders/config/SpringSecConfig.kt
@@ -2,20 +2,46 @@ package com.kotlin.orders.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SpringSecConfig {
 
-    @Bean
-    fun filterchain(http: HttpSecurity): SecurityFilterChain{
-        http.authorizeHttpRequests()
-            .anyRequest().authenticated()
-            .and().httpBasic(Customizer.withDefaults())
-            .csrf().ignoringRequestMatchers("/orders/**")
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeRequests {
+                authorize("/orders", hasAuthority("SCOPE_orders.read"))
+                authorize(anyRequest, authenticated)
+            }
+            formLogin{
+                loginPage = "/login"
+            }
+        }
         return http.build()
+    }
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+        val userDetails = User.builder()
+            .username("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .build()
+
+        val adminDetails = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("adminpassword"))
+            .roles("ADMIN")
+            .build()
+        return InMemoryUserDetailsManager(userDetails, adminDetails)
     }
 
 }
